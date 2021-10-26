4213c9f3e30b52fc36eee1c35836da77fba624b7
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-10-25 23:39:30 -0300

    arrumacolunas

--- resources/views/livros/partials/emprestimos.blade.php
+++ resources/views/livros/partials/emprestimos.blade.php
@@ -14,9 +14,8 @@
     @foreach($livro->emprestimos as $emprestimo)
     <tr>
       <td> <img src="/foto/{{ $emprestimo->usuario->matricula }}" width="100px"> </td>
-      <td><a href="/usuarios/{{ $emprestimo->usuario->id }}">{{ $emprestimo->usuario->nome }}</a></td>
       <td>{{ $emprestimo->usuario->matricula }}</td>
-      <td>{{ $emprestimo->usuario->nome }}</td>
+      <td><a href="/usuarios/{{ $emprestimo->usuario->id }}">{{ $emprestimo->usuario->nome }}</a></td>
       <td>{{ $emprestimo->data_emprestimo }}</td>
       <td>{{ $emprestimo->data_devolucao }}</td>
       <td>{{ $emprestimo->dias }}</td>
