56f2d37200bbe4b093dc4ceb681e67c479fd81c1
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2023-03-31 11:47:53 -0300

    audioria tcc

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use \App\Models\Tcc;
 
-class File extends Model
+use OwenIt\Auditing\Contracts\Auditable;
+
+class File extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
 
     public function tccs(){
         return $this->belongsTo(Tcc::class);
--- app/Models/Tcc.php
+++ app/Models/Tcc.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\File;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Tcc extends Model
+class Tcc extends Model implements Auditable
 {
     use HasFactory;
+    use \OwenIt\Auditing\Auditable;
+
     protected $guarded = ['id'];
 
     public function files()
